32b16066d406c8de68b87dc221700f4606f29bef
小怡酱c <879373096@qq.com>
2023-05-28 18:49:27 +0800

    project status

--- src/components/projectshub.vue
+++ src/components/projectshub.vue
@@ -6,11 +6,13 @@
             <el-icon style="position: absolute;right:4rem;top:2rem;font-size:3rem;color: #f2f2f2;" @click="add_project=true"><Plus /></el-icon>
         </span>
         <div class="userhub--projectlist">
-            <span class="userhub--projectlist--item" v-for="item in currentprojectlist" :key="item.name">
-                <span class="userhub--projectlist--item--icon"><el-icon v-if="item.type =='cv'"><Picture /></el-icon><el-icon v-else-if="item.type =='mal'"><Platform /></el-icon ><el-icon v-else-if="item.type =='eval'"><DataAnalysis /></el-icon><el-icon v-else-if="item.type =='reinforce'"><svg t="1685245817415"  viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4321" ><path d="M409.6 750.933l34.133 68.267H170.667v136.533h682.666V819.2H580.267l34.133-68.267h238.933A68.267 68.267 0 0 1 921.6 819.2v136.533A68.267 68.267 0 0 1 853.333 1024H170.667a68.267 68.267 0 0 1-68.267-68.267V819.2a68.267 68.267 0 0 1 68.267-68.267H409.6zM273.067 68.267h477.866a68.267 68.267 0 0 1 68.267 68.266V614.4a68.267 68.267 0 0 1-68.267 68.267H273.067A68.267 68.267 0 0 1 204.8 614.4V136.533a68.267 68.267 0 0 1 68.267-68.266z m0 68.266V614.4h477.866V136.533H273.067z m614.4 102.4a34.133 34.133 0 0 1 34.133 34.134v204.8a34.133 34.133 0 1 1-68.267 0v-204.8a34.133 34.133 0 0 1 34.134-34.134z m-750.934 0a34.133 34.133 0 0 1 34.134 34.134v204.8a34.133 34.133 0 0 1-68.267 0v-204.8a34.133 34.133 0 0 1 34.133-34.134zM989.867 307.2A34.133 34.133 0 0 1 1024 341.333V409.6a34.133 34.133 0 1 1-68.267 0v-68.267a34.133 34.133 0 0 1 34.134-34.133z m-955.734 0a34.133 34.133 0 0 1 34.134 34.133V409.6A34.133 34.133 0 0 1 0 409.6v-68.267A34.133 34.133 0 0 1 34.133 307.2z m341.334 102.4a51.2 51.2 0 1 0 0-102.4 51.2 51.2 0 0 0 0 102.4z m273.066 0a51.2 51.2 0 1 0 0-102.4 51.2 51.2 0 0 0 0 102.4zM512 0a34.133 34.133 0 0 1 34.133 34.133V102.4a34.133 34.133 0 0 1-68.266 0V34.133A34.133 34.133 0 0 1 512 0z m-68.267 614.4a34.133 34.133 0 0 1 34.134 34.133v136.534a34.133 34.133 0 1 1-68.267 0V648.533a34.133 34.133 0 0 1 34.133-34.133z m136.534 0a34.133 34.133 0 0 1 34.133 34.133v136.534a34.133 34.133 0 1 1-68.267 0V648.533a34.133 34.133 0 0 1 34.134-34.133z" p-id="4322"></path></svg></el-icon ></span>
-                <span class="userhub--projectlist--item--text" @click="emits('switch_to_penel',item)">
+            <span class="userhub--projectlist--item"  v-for="item in currentprojectlist" :key="item.name">
+                <span class="userhub--projectlist--item--icon" :class="{'u-background-color-active':item.status,'u-border-color-active':item.status}"><el-icon v-if="item.type =='cv'"><Picture /></el-icon><el-icon v-else-if="item.type =='mal'"><Platform /></el-icon ><el-icon v-else-if="item.type =='eval'"><DataAnalysis /></el-icon><el-icon v-else-if="item.type =='reinforce'"><svg t="1685245817415"  viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4321" ><path d="M409.6 750.933l34.133 68.267H170.667v136.533h682.666V819.2H580.267l34.133-68.267h238.933A68.267 68.267 0 0 1 921.6 819.2v136.533A68.267 68.267 0 0 1 853.333 1024H170.667a68.267 68.267 0 0 1-68.267-68.267V819.2a68.267 68.267 0 0 1 68.267-68.267H409.6zM273.067 68.267h477.866a68.267 68.267 0 0 1 68.267 68.266V614.4a68.267 68.267 0 0 1-68.267 68.267H273.067A68.267 68.267 0 0 1 204.8 614.4V136.533a68.267 68.267 0 0 1 68.267-68.266z m0 68.266V614.4h477.866V136.533H273.067z m614.4 102.4a34.133 34.133 0 0 1 34.133 34.134v204.8a34.133 34.133 0 1 1-68.267 0v-204.8a34.133 34.133 0 0 1 34.134-34.134z m-750.934 0a34.133 34.133 0 0 1 34.134 34.134v204.8a34.133 34.133 0 0 1-68.267 0v-204.8a34.133 34.133 0 0 1 34.133-34.134zM989.867 307.2A34.133 34.133 0 0 1 1024 341.333V409.6a34.133 34.133 0 1 1-68.267 0v-68.267a34.133 34.133 0 0 1 34.134-34.133z m-955.734 0a34.133 34.133 0 0 1 34.134 34.133V409.6A34.133 34.133 0 0 1 0 409.6v-68.267A34.133 34.133 0 0 1 34.133 307.2z m341.334 102.4a51.2 51.2 0 1 0 0-102.4 51.2 51.2 0 0 0 0 102.4z m273.066 0a51.2 51.2 0 1 0 0-102.4 51.2 51.2 0 0 0 0 102.4zM512 0a34.133 34.133 0 0 1 34.133 34.133V102.4a34.133 34.133 0 0 1-68.266 0V34.133A34.133 34.133 0 0 1 512 0z m-68.267 614.4a34.133 34.133 0 0 1 34.134 34.133v136.534a34.133 34.133 0 1 1-68.267 0V648.533a34.133 34.133 0 0 1 34.133-34.133z m136.534 0a34.133 34.133 0 0 1 34.133 34.133v136.534a34.133 34.133 0 1 1-68.267 0V648.533a34.133 34.133 0 0 1 34.134-34.133z" p-id="4322"></path></svg></el-icon ></span>
+                <span class="userhub--projectlist--item--text" :class="{'u-border-color-active':item.status}"  @click="emits('switch_to_penel',item)">
                     {{ item.name }}
                     <span class="userhub--projectlist--item--text--icon-box">
+                        <el-icon class="userhub--projectlist--item--text--icon-box__icon" @click.stop="item.status = true"><CaretRight /></el-icon>
+                        <el-icon class="userhub--projectlist--item--text--icon-box__icon" @click.stop="item.status = false"><SwitchButton /></el-icon>
                         <el-icon class="userhub--projectlist--item--text--icon-box__icon" @click.stop="open(item)"><DeleteFilled /></el-icon>
                         <el-icon class="userhub--projectlist--item--text--icon-box__icon" style="transform: rotate(90deg);"><MoreFilled /></el-icon>
                     </span>
@@ -98,8 +100,11 @@ const props = defineProps<{
 const emits = defineEmits(["set_projects","switch_to_penel"]);
 const name:Ref<string> = ref('肆夕')
 const titleList: Ref<string[]> = ref(['Student', 'SCU'])
-const testprojectlist:Ref<Project[]> = ref(props.userMessage.projects)
-let currentprojectlist:Ref<Project[]> = ref([])
+const testprojectlist: Ref<any[]> = ref(props.userMessage.projects.map((value, index) => {
+    value['status'] = false;
+    return value
+}))
+let currentprojectlist:Ref<ProjectPlus[]> = ref([])
 const currentpage:Ref<number> = ref(1)
 const testcursor = ref(11)
 const totalPage:Ref<number> = ref(Math.ceil(testprojectlist.value.length / 12));
@@ -133,9 +138,11 @@ function testlastpage() {
         getCurrentPageData()
       }
 }
-interface testProject {
+interface ProjectPlus {
     type: string;
     name: string;
+    id: string;
+    status: boolean;
 }
 interface Project {
     type: string;
